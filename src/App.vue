9df95399bf9d70ad9a7d56d6aa1fd59d38489d17
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-26 13:26:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppMain from './components/AppMain.vue'
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'App',
+  components: { AppHeader, AppMain },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+<AppHeader></AppHeader>
+<AppMain></AppMain>
+</template>
+
+<style scoped>
+
+</style>
