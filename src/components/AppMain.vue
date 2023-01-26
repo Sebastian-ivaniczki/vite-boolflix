b3dbbf44509ef6f3a8605e68d2101f86d9d49a35
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-26 18:30:58 +0100

    added posters

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,8 +4,8 @@ export default {
     name: 'AppMain',
     data() {
         return{
-           
-            store
+           store, 
+           imgSize: 'https://image.tmdb.org/t/p/w342/'
         }
     },
    
@@ -14,10 +14,11 @@ export default {
 
 <template>
 <ul>
-    <li v-for="movie in store.moviesList" :key="movie.id">
+    <li class="my-5" v-for="movie in store.moviesList" :key="movie.id">
         <div>
             <h2>{{ movie.title }}</h2>
             <h3>{{ movie.original_title }}</h3>
+            <img :src="imgSize + movie.poster_path" :alt=" movie.title">
             
             <img :src="`/src/assets/${movie.original_language}.png`" :alt="movie.original_language">
             <span>{{ movie.vote_average }}</span>
@@ -27,10 +28,11 @@ export default {
 <h1 class="my-5 text-danger">series</h1>
 
 <ul>
-    <li v-for="serie in store.seriesList" :key="serie.id">
+    <li class="my-5" v-for="serie in store.seriesList" :key="serie.id">
         <div>
             <h2>{{ serie.name }}</h2>
             <h3>{{ serie.original_name }}</h3>
+            <img :src="imgSize + serie.poster_path" :alt="serie.name">
             
             <img :src="`/src/assets/${serie.original_language}.png`" :alt="serie.original_language">
             <span>{{ serie.vote_average }}</span>
