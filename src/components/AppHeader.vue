ce7915b300a4f856dd4ad6499a12718e89a4fa05
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-26 15:11:05 +0100

    added searched store

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,21 @@
 <script>
+import { store } from './data/store'
 export default {
     name: 'AppHeader',
     data() {
         return{
-            serch:''
+           
+            store
         }
-    }
+    },
+    emits:[ 'searchedText' ] 
+       
 }
 </script>
 
 <template>
 <div class="input-group mb-3">
-  <input v-model="serch" type="text" class="form-control" placeholder="inserisci nome film">
+  <input @keyup.enter="$emit('searchedText', store.searchedText)" v-model="store.searchedText" type="text" class="form-control" placeholder="inserisci nome film">
   <button class="btn btn-outline-secondary">Button</button>
 </div>
 </template>
--- src/components/data/store.js
+++ src/components/data/store.js
@@ -2,7 +2,7 @@ import { reactive } from 'vue';
 
 //espemio export
 export const store = reactive ({
-    
+    searchedText: '',
     moviesList: [],
    
 });
