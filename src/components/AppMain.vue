85374ae98928820c9de6ecbc1788a5eae8f2a062
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-26 17:36:19 +0100

    fix img

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,7 +19,7 @@ export default {
             <h2>{{ movie.title }}</h2>
             <h3>{{ movie.original_title }}</h3>
             
-            <img :src="`../assets/${movie.original_language}.png`" alt="">
+            <img :src="`/src/assets/${movie.original_language}.png`" alt="">
             <span>{{ movie.vote_average }}</span>
         </div>
     </li>
