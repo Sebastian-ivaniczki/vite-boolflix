1a0e07d5c30010ce1b9da7e5aa17bea686ce9ca4
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-26 14:09:28 +0100

    add serchbar in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,19 @@
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    data() {
+        return{
+            serch:''
+        }
+    }
 }
 </script>
 
 <template>
-
+<div class="input-group mb-3">
+  <input v-model="serch" type="text" class="form-control" placeholder="inserisci nome film">
+  <button class="btn btn-outline-secondary">Button</button>
+</div>
 </template>
 
 <style scoped>
